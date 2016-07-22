feb1684d80dbcfb121eb72b4cfbe93bb0613c2bd
yhwang <yihongwang@gmail.com>
2016-07-22 23:49:05 +0800

    Reformt JavaScript code
    Reformat the JavaScript source code, in order to compliant
    with eslint style.

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
 /*******************************************************************************
-* Copyright (c) 2015 IBM Corp.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright (c) 2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 /*eslint-env node */
 'use strict';
 
-var console = require('console');
-var config  = require('./config');
-var sys     = require('util');
-var nats    = require('nats').connect();
-var exec    = require('child_process').exec;
+var sys = require('util');
+var nats = require('nats').connect();
+var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
-var soap    = require('soap');
-var crypto  = require('crypto');
-var net     = require('net');
-var fs      = require('fs');
-var Buffer  = require('buffer').Buffer;
-var path    = require('path');
-var watson  = require('watson-developer-cloud');
+var soap = require('soap');
+var crypto = require('crypto');
+var net = require('net');
+var fs = require('fs');
+var Buffer = require('buffer').Buffer;
+var path = require('path');
+var watson = require('watson-developer-cloud');
 //var Sound   = require('node-aplay'); 
 var HumixSense = require('node-humix-sense');
+var log = require('humix-logger').createLogger('humix-dialog-module', {
+  consoleLevel : 'debug'
+});
+
 var HumixSpeech = require('./lib/HumixSpeech').HumixSpeech;
+var config = require('./config');
 
 var voice_path = path.join(__dirname, 'voice');
 var url = 'http://tts.itri.org.tw/TTSService/Soap_1_3.php?wsdl';
-var kGoogle = 0,
-    kWatson = 1;
+var kGoogle = 0;
+var kWatson = 1;
 
 var OpenCC = require('opencc');
 var opencc = new OpenCC('s2t.json');
 
-var engineIndex = {'google': kGoogle, 'watson': kWatson };
+var engineIndex = {
+  'google' : kGoogle,
+  'watson' : kWatson
+};
 
 var ttsWatson;
 var retry = 0;
@@ -50,35 +56,32 @@ var retry = 0;
 var TTS_busy = false;
 
 if (config['tts-engine'] === 'watson') {
-    ttsWatson = watson.text_to_speech({
-        'username': config.tts.watson.username,
-        'password': config.tts.watson.passwd,
-        version: 'v1',
-    });
+  ttsWatson = watson.text_to_speech({
+    username : config.tts.watson.username,
+    password : config.tts.watson.passwd,
+    version : 'v1',
+  });
 }
 
 var moduleConfig = {
-    "moduleName":"humix-dialog",
-    "commands" : ["say"],
-    "events" : ["speech"],
-    "debug": true
+  moduleName : 'humix-dialog',
+  commands : [ 'say' ],
+  events : [ 'speech' ],
+  debug : true
 }
 
 var humix = new HumixSense(moduleConfig);
 var hsm;
 
-humix.on('connection', function(humixSensorModule){
+humix.on('connection', function(humixSensorModule) {
 
-    hsm = humixSensorModule;
-
-    console.log('Communication with humix-sense is now ready.');
-
-    hsm.on('say', function(data){
-        console.log('data:'+data);
-        text2Speech(data);
-    });  // end of say command
+  hsm = humixSensorModule;
+  log.debug('Communication with humix-sense is now ready.');
+  hsm.on('say', function(data) {
+    log.debug('data:', data);
+    text2Speech(data);
+  }); // end of say command
 });
-
 
 /* 
  * Speech To Text Processing
@@ -95,249 +98,247 @@ var commandRE = /---="(.*)"=---/;
  *         '----="command string"=---'
  */
 function receiveCommand(cmdstr) {
-    cmdstr = cmdstr.trim();
-    if ( config['stt-engine'] ) {
-        console.log('command found:', cmdstr);
-        
-        if(hsm){
+  cmdstr = cmdstr.trim();
+  if (config['stt-engine']) {
+    log.debug('command found:', cmdstr);
 
-            if(config.lang == 'cht')
-                hsm.event("speech", opencc.convertSync(cmdstr));
-            else
-                hsm.event("speech", cmdstr)
-        }
-    } else {
-        
-        console.log(" No stt engine configured. Skip");
-        
+    if (hsm) {
+      if (config.lang == 'cht')
+        hsm.event('speech', opencc.convertSync(cmdstr));
+      else
+        hsm.event('speech', cmdstr)
     }
+  } else {
+    log.debug('No stt engine configured. Skip');
+  }
 }
-
 
 try {
-    hs = new HumixSpeech(config.options);
-    var engine = config['stt-engine'] || 'watson';
-    hs.engine( config.stt[engine].username, config.stt[engine].passwd,
-    		engineIndex[engine], require('./lib/' + engine).startSession);
-    hs.start(receiveCommand);
-} catch ( error ) {
-    console.error(error);
+  var defaultOpts = {
+    vad_threshold: '3.5',
+    upperf: '1000',
+    hmm: './deps/pocketsphinx-5prealpha/model/en-us/en-us',
+    //http://www.speech.cs.cmu.edu/tools/lmtool.html use this website
+    //to generate lm and dict file based on your keyword-name
+    lm: './lm/humix.lm',
+    dict: './lm/humix.dic',
+    'keyword-name': 'HUMIX', //all capital characters
+    samprate: '16000',
+    maxwpf: '5',
+    topn: '2',
+    maxhmmpf: '3000',
+    pl_window: '7',
+    ds: '2',
+    cmdproc: './util/processcmd.sh',
+    lang: 'zh-tw',
+    'wav-proc': './voice/interlude/beep2.wav',
+    'wav-bye': './voice/interlude/beep1.wav',
+    logfn: '/dev/null'
+  };
+  var options = config.options || {};
+  for(var option in options) {
+    defaultOpts[option] = options[option];
+  }
+  hs = new HumixSpeech(defaultOpts);
+  var engine = config['stt-engine'] || 'watson';
+  hs.engine(config.stt[engine].username, config.stt[engine].passwd,
+      engineIndex[engine], require('./lib/' + engine).startSession);
+  hs.start(receiveCommand);
+} catch (error) {
+  log.error(error);
 }
-
 
 /* 
  * Text To Speech Processing
  */
-
 function text2Speech(msg) {
-    console.log('Received a message:', msg);
-    var text
-    var wav_file = '';
+  log.debug('Received a message:', msg);
+  var text
+  var wav_file = '';
+  try {
+    text = JSON.parse(msg).text;
+  } catch (e) {
+    log.error('invalid JSON format,', e);
+    return;
+  }
+
+  if (!text) {
+    return log.error('Missing property: msg.text');
+  }
+
+  //for safe
+  text = text.trim();
+
+  var person = 'xiaoyan'
+  if (config['tts-engine'] === 'iflytek') {
     try {
-        text = JSON.parse(msg).text;
+      person = JSON.parse(msg).person;
     } catch (e) {
-        console.error('invalid JSON format. Skip');
-        return;
+      person = 'xiaoyan';
     }
+    IflytekTTS(text, person);
+    return;
+  }
 
-    if (!text) {
-        return console.error('Missing property: msg.text');
-    }
+  var hash = crypto.createHash('md5').update(text).digest('hex');
+  var filename = path.join(voice_path, 'text', hash + '.wav');
 
-    //for safe
-    text = text.trim();
-    
-	var person = 'xiaoyan'
-		if ( config['tts-engine'] === 'iflytek') {
-		    try {
-        		person = JSON.parse(msg).person;
-    		}	catch (e) {
-        		person='xiaoyan';
-    		}	
-            IflytekTTS(text, person);
-            return;         
-    }
-      
-    
+  if (fs.existsSync(filename)) {
+    log.debug('Wav file exist. Play cached file:', filename);
+    sendAplay2HumixSpeech(filename);
+  } else {
+    log.debug('Wav file does not exist');
+    var ttsEngine = config['tts-engine'];
 
-    var hash = crypto.createHash('md5').update(text).digest('hex');
-    var filename = path.join(voice_path, 'text', hash + '.wav');
-         
-   
-    if(fs.existsSync(filename)){
-        
-        console.log('Wav file exist. Play cached file:', filename);
-        sendAplay2HumixSpeech(filename);
-    } else {
-
-        console.log('Wav file does not exist');
-      
-        var ttsEngine = config['tts-engine'];
-
-        console.log('tts-engine:', engine);            
-        if ( ttsEngine === 'itri') {
-            console.log("username : "+config.tts.itri.username);
-            console.log("password : "+config.tts.itri.passwd);
-            ItriTTS(text, function(err, id) {
-                if (err) {
-
-                    console.log('failed to download wav from ITRI. Error:' + err);
-
-                } else {
-                            
-                    retry = 0;
-                    setTimeout(ItriDownload, 1000, id, filename);
-                }
-            });
-
-        }else if ( ttsEngine === 'watson') {
-
-            WatsonTTS(text,filename);             
+    log.debug('tts-engine:', engine);
+    if (ttsEngine === 'itri') {
+      log.debug('username :', config.tts.itri.username);
+      ItriTTS(text, function(err, id) {
+        if (err) {
+          log.error('failed to download wav from ITRI. Error:', err);
+        } else {
+          retry = 0;
+          setTimeout(ItriDownload, 1000, id, filename);
         }
-      
+      });
+    } else if (ttsEngine === 'watson') {
+      WatsonTTS(text, filename);
     }
-   
+  }
 }
-
-
 
 /**
  * call the underlying HumixSpeech to play wave file
  * @param file wave file
  */
-function sendAplay2HumixSpeech( file ) {
-    if( hs ) {
-        hs.play(file);
-    }
+function sendAplay2HumixSpeech(file) {
+  if (hs) {
+    hs.play(file);
+  }
 }
-
 
 /* 
  * Watson TTS Processing
  */
-
-function WatsonTTS(text,filename) {
-      
-    ttsWatson.synthesize({ 'text' : text , 'accept': 'audio/wav'}, function(err) {
-        
-        if(err){
-            
-            console.err("error:" + err);
-            return;
-        }
-        fs.writeFileSync( filename, new Buffer(arguments[1]));
-        sendAplay2HumixSpeech(filename);
-    });
+function WatsonTTS(text, filename) {
+  ttsWatson.synthesize({
+    text : text,
+    accept : 'audio/wav'
+  }, function(err) {
+    if (err) {
+      log.error('error:', err);
+      return;
+    }
+    fs.writeFileSync(filename, new Buffer(arguments[1]));
+    sendAplay2HumixSpeech(filename);
+  });
 }
-
 
 /* 
  * Iflytek TTS Processing
  */
-
 function IflytekTTS(text, person) {
-      
-    if(!TTS_busy){
-        TTS_busy = true;
-        var execFile = "./tts";
-        var spawn = require("child_process").spawn;
-        var free = spawn(execFile, [text, 'tts.wav', person]);
-        free.stdout.on("data", function(data){console.log("out" + data);});
-        free.stderr.on("data", function(data){console.log("err" + data);});
-        free.on("exit", function(code, signal){TTS_busy = false; console.log("exit" + code);});
-    }else{
-        console.log("TTS is Busy!");
-    }
+  if (!TTS_busy) {
+    TTS_busy = true;
+    var execFile = './tts';
+    var spawn = require('child_process').spawn;
+    var free = spawn(execFile, [ text, 'tts.wav', person ]);
+    free.stdout.on('data', function(data) {
+      log.debug('out', data);
+    });
+    free.stderr.on('data', function(data) {
+      log.debug('err', data);
+    });
+    free.on('exit', function(code, signal) {
+      TTS_busy = false;
+      log.debug('exit', code);
+    });
+  } else {
+    log.error('TTS is Busy!');
+  }
 }
-
 
 /* 
  * ITRI TTS Processing
  */
-
 function ItriTTS(text, callback) {
-    var args = {
-        accountID: config.tts.itri.username,
-        password: config.tts.itri.passwd,
-        TTStext: text,
-        TTSSpeaker: config.tts.itri.speaker,
-        volume: 50,
-        speed: -2,
-        outType: 'wav'
-    };
-    soap.createClient(url, function(err, client) {
-        client.ConvertText(args, function(err, result) {
-            if (err) {
-                console.log('err:', err);
-                callback(err, null);
-            }
-            try {
-                var id = result.Result.$value.split('&')[2];
-                if (id) {
-                    console.log('get id:', id);
-                    callback(null, id);
-                } else {
-                    throw 'failed to convert text!';
-                }
-            } catch (e) {
-                console.log(error);
-                callback(error, null);
-            }
-        });
+  var args = {
+    accountID : config.tts.itri.username,
+    password : config.tts.itri.passwd,
+    TTStext : text,
+    TTSSpeaker : config.tts.itri.speaker,
+    volume : 50,
+    speed : -2,
+    outType : 'wav'
+  };
+  soap.createClient(url, function(err, client) {
+    client.ConvertText(args, function(err, result) {
+      if (err) {
+        log.error('err:', err);
+        callback(err, null);
+      }
+      try {
+        var id = result.Result.$value.split('&')[2];
+        if (id) {
+          log.debug('get id:', id);
+          callback(null, id);
+        } else {
+          throw 'failed to convert text!';
+        }
+      } catch (e) {
+        log.error(error);
+        callback(error, null);
+      }
     });
+  });
 }
 
 function ItriGetConvertStatus(id, filename, callback) {
-    var args = {
-        //accountIDhasOwnProperty: config.tts.itri.username,
-        accountID: config.tts.itri.username,
-        password: config.tts.itri.passwd,
-        convertID: id
-    };
-    soap.createClient(url, function(err, client) {
-        console.log('msg_id', id);
-        client.GetConvertStatus(args, function(err, result) {
-            if (err) {
-                console.log('err:', err);
-                callback(err, null);
-            }
-            var downloadUrl = result.Result.$value.split('&')[4];
-            if (downloadUrl) {
-               
-                console.log(id, downloadUrl);
-                execSync('wget '+ downloadUrl + ' -O ' + filename, {stdio: [ 'ignore', 'ignore', 'ignore' ]});
-                callback(null, filename);
-            } else {
-                var error = 'Still converting! result: '+JSON.stringify(result);
-                console.log(error);
-                callback(error, null);
-            }
+  var args = {
+    //accountIDhasOwnProperty: config.tts.itri.username,
+    accountID : config.tts.itri.username,
+    password : config.tts.itri.passwd,
+    convertID : id
+  };
+  soap.createClient(url, function(err, client) {
+    log.debug('msg_id', id);
+    client.GetConvertStatus(args, function(err, result) {
+      if (err) {
+        log.error('err:', err);
+        callback(err, null);
+      }
+      var downloadUrl = result.Result.$value.split('&')[4];
+      if (downloadUrl) {
+
+        log.debug(id, downloadUrl);
+        execSync('wget ' + downloadUrl + ' -O ' + filename, {
+          stdio : [ 'ignore', 'ignore', 'ignore' ]
         });
+        callback(null, filename);
+      } else {
+        var error = 'Still converting! result: ' + JSON.stringify(result);
+        log.error(error);
+        callback(error, null);
+      }
     });
+  });
 }
 
-
-function ItriDownload (id, filename) {
-    retry++;
-    ItriGetConvertStatus(id, filename, function(err) {
-        if (err) 
-        { 
-            console.log('err:', err); 
-            if (retry < 10)
-            {
-               console.log('retry', retry);
-               setTimeout(ItriDownload, 2000, id, filename);
-            }
-        }
-        else 
-        {
-           console.log('Play wav file:', filename);
-           sendAplay2HumixSpeech(filename);
-        }
-    });
+function ItriDownload(id, filename) {
+  retry++;
+  ItriGetConvertStatus(id, filename, function(err) {
+    if (err) {
+      log.error('err:', err);
+      if (retry < 10) {
+        log.debug('retry', retry);
+        setTimeout(ItriDownload, 2000, id, filename);
+      }
+    } else {
+      log.debug('Play wav file:', filename);
+      sendAplay2HumixSpeech(filename);
+    }
+  });
 }
-
-
 
 /* 
  * Signal Handling
@@ -345,33 +346,33 @@ function ItriDownload (id, filename) {
 
 process.stdin.resume();
 function cleanup() {
-    if (hs) {
-        hs.stop();
-    }
+  if (hs) {
+    hs.stop();
+  }
 }
 process.on('SIGINT', function() {
-    cleanup();
-    process.exit(0);
+  cleanup();
+  process.exit(0);
 });
 process.on('SIGHUP', function() {
-    cleanup();
-    process.exit(0);
+  cleanup();
+  process.exit(0);
 });
 process.on('SIGTERM', function() {
-    cleanup();
-    process.exit(0);
+  cleanup();
+  process.exit(0);
 });
 process.on('exit', function() {
-    cleanup();
+  cleanup();
 });
 process.on('error', function() {
-    cleanup();
+  cleanup();
 });
 
 process.on('uncaughtException', function(err) {
-    if ( err.toString().indexOf('connect ECONNREFUSED') ) {
-        console.error('exception,', JSON.stringify(err));
-        //cleanup();
-        //process.exit(0);
-    }
+  if (err.toString().indexOf('connect ECONNREFUSED')) {
+    log.error('exception,', JSON.stringify(err));
+    //cleanup();
+    //process.exit(0);
+  }
 });
